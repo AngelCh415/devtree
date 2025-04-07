56c71225e70496ef5442bb4c99ea96e1e55529c9
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-04-07 09:48:35 -0600

    feat: Add env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express' // ESM Ecamascript Module
+import 'dotenv/config' // Load environment variables from .env file
 import router from './router' // Import the router module
+import { connectDB } from './config/db'
 
 // Initialize the express application
 const app = express()
+connectDB() // Connect to the database
 
 // Read data of forms
 app.use(express.json())
