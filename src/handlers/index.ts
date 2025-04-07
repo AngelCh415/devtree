6a9f3435dccf5f7ec335c6e0885ab9677652d843
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-04-07 12:09:03 -0600

    feat: Add auth

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,17 @@
 import type{ Request, Response } from 'express'
+import { validationResult } from 'express-validator'
 import slug from 'slug'
 import User from "../models/User"
 import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-    
+    // Errors control
+    let errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.status(400).json({
+            errors: errors.array(),
+        })
+    }
     const {email, password} = req.body
     // FindOne == where with one coincidence
     const userExists = await User.findOne({email})   
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './handlers'
 
 const router = Router()
 
 // Routing - Get
-router.post('/auth/register', createAccount)
+router.post('/auth/register',
+    body('handle').notEmpty().withMessage('Handle must not be empty'),
+    body('name').notEmpty().withMessage('Name must not be empty'),
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters'),
+    createAccount)
 
 export default router
