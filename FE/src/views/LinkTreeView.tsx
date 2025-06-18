d61fd8b4818c4405ed023042bec7bee7152c406d
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-06-18 12:55:59 -0600

    feat: Add input of links

--- FE/src/views/LinkTreeView.tsx
+++ FE/src/views/LinkTreeView.tsx
@@ -4,12 +4,19 @@ import DevTreeInput from "../components/DevTreeInput";
 
 export default function LinkTreeView() {
     const [devTreeLinks, setDevTreeLinks] = useState(social);
+
+    const handelUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        console.log(e.target.name);
+
+    }
     return (
             <div className="space-y-5">
                 {devTreeLinks.map(item => (
                     <DevTreeInput
                         key={item.name}
                         item={item}
+                        handelUrlChange={handelUrlChange}
                     />
                 ))}
 
